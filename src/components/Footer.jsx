a5408d7929ce9b6a532e0f028e24d025c2c662d0
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-12-05 17:52:59 -0700

    added css styling but I need to fix it

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import { ImFacebook, ImInstagram, ImTwitter, ImGithub } from "react-icons/im";
+import styles from  '../UI/Footer.module.css'
 
 const Footer = () => {
   return (
     <footer>
       <div>
         <h5>Hit us up on our socials!</h5>
-        <div>
+        <div className="container">
+          <div>
           <a href="https://www.devmountain.com" target="_blank">
-            <ImFacebook size="1.5em" color="#3f3f3f" />
+            <ImFacebook size="1.5em" color="#85CBAF" />
           </a>
+          </div>
+          <div className="footer-icon">
           <a href="https://www.devmountain.com" target="_blank">
-            <ImInstagram size="1.5em" color="#3f3f3f" />
+            <ImInstagram size="1.5em" color="#85CBAF" />
           </a>
+          </div>
+          <div className={styles.icon}>
           <a href="https://www.devmountain.com" target="_blank">
-            <ImTwitter size="1.5em" color="#3f3f3f" />
+            <ImTwitter size="1.5em" color="#85CBAF" />
           </a>
+          </div >
+          <div className={styles.icon}>
           <a href="https://www.devmountain.com" target="_blank">
-            <ImGithub size="1.5em" color="#3f3f3f" />
+            <ImGithub size="1.5em" color="#85CBAF" />
           </a>
+          </div>
         </div>
       </div>
       <svg
